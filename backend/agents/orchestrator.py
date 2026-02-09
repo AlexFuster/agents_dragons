0a57138c9208e3e1912ead6b9a1cf2ac58a25adf
Alex <alejandro.fuster@bravent.net>
2026-02-09 17:28:46 +0100

    add usage

--- backend/agents/orchestrator.py
+++ backend/agents/orchestrator.py
@@ -1,10 +1,14 @@
 from common.agent import Agent
-from agent_framework import ai_function
+try:
+    from agent_framework import tool
+except ImportError:
+    from agent_framework import ai_function
+    tool = ai_function        
 
 class OrchestratorAgent(Agent):
     def __init__(self, orchestrated_agents: list):
         super().__init__(
                 name="Orchestrator",
                 description="The agent that narrates the story based on the user input and the story context.",
-                tools=[ai_function(agent.run, name=agent.name, description=agent.description) for agent in orchestrated_agents]
+                tools=[tool(agent.run, name=agent.name, description=agent.description) for agent in orchestrated_agents]
             )
--- backend/common/agent.py
+++ backend/common/agent.py
@@ -1,31 +1,40 @@
 import os
 from common.openai_client import get_openai_client
 from common.logging import config_logging
+from agent_framework import ChatAgent
+import json
 
-cuur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
             
 class Agent:
     def __init__(self, name: str, description: str, tools: list = [], threading: bool = True):
         openai_client = get_openai_client()
-        with open(f'{cuur_dir}/prompts/{name}.md', 'r') as f:
-            instructions = f.read()
         self.name = name
         self.description = description
-        self.agent = openai_client.create_agent(
-                        name=name,
-                        description=description,
-                        instructions=instructions,
-                        tools=tools
-                    )
+        
+        self.logger = config_logging(self.name)
+        try:
+            with open(f'{root_dir}/prompts/{name}.md', 'r') as f:
+                instructions = f.read()
+        except FileNotFoundError:
+            instructions = "No instructions provided."
+            self.logger.error(f"No instructions found") 
+
+        self.agent = ChatAgent(
+            chat_client=openai_client,
+            name=name,
+            description=description,
+            instructions=instructions,
+            tools=tools
+        )
         if threading:
             self.thread = self.agent.get_new_thread()
         else:
             self.thread = None
-            
-        self.logger = config_logging(self.name)
         
     async def run(self, input: str) -> str:
         self.logger.info(f"Received input: {input}")
         response = await self.agent.run(input, thread=self.thread)
         self.logger.info(f"Generated response: {response.text}")
+        self.logger.info(f"Usage details: {response.usage_details}")
         return response.text
